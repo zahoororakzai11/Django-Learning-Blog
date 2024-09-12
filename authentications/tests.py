8d0f7e229a609af3ca69c32ebde9093e58d2dd8d
zahoororakzai11 <zahoor.orakzai11@gmail.com>
2024-09-12 12:54:43 +0500

    All

--- authentications/tests.py
+++ authentications/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Blog,Author
 
-# Create your tests here.
+class AuthorTest(TestCase):
+    
+    def setUp(self):
+        Blog.objects.create(name="Tech Blog",tagline="All About Tech Blog")
+        # Blog.objects.create(name='Medical Blog',tagline="All about Medical Blog")
+    
+    def test_blog_creation(self):
+        blog = Blog.objects.get(name="Tech Blog")
+        self.assertEqual(blog.tagline,"All About Tech Blog")
+    
+    def test_auth_str_creation(self):
+        blog = Blog.objects.get(name='Tech Blog')
+        self.assertEqual(str(blog),'Tech Blog')
--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.settings')
+>>>>>>> eeaa7fb (All)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
